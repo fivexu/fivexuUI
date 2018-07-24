86169d335ccded549207ef0e9b1c39c3ccc61e8d
fivexu <316643470@qq.com>
2018-07-24 19:11:33 +0800

    fix timeLine

--- src/base/timeLine/timeLine.vue
+++ src/base/timeLine/timeLine.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="time_line" ref="timeLineWrapper">
-        <div class="left" ref="timeLineLeft">
+        <div class="left" ref="timeLineLeft" @click.stop="_prevent">
             <slot name="left"></slot>
         </div>
         <div class="content" ref="timeLineContent">
             <slot name="content"></slot>
         </div>
-        <div class="right" ref="timeLineRight">
+        <div class="right" ref="timeLineRight" @click.stop="_next">
             <slot name="right"></slot>
         </div>
     </div>
@@ -19,29 +19,78 @@
         extends: GlobalForm,
         name: 'timeLine',
         props: {
+            data: {
+                type: Array,
+                default: []
+            },
             option: {
                 type: Object,
                 default: () => {
                     return {
-                        showNumber: 3
+                        runNumber: 2
                     }
                 }
             }
         },
         data() {
-            return {}
+            return {
+                currentIndex: 0
+            }
         },
         methods: {
+            _getStyle(obj, attr) {
+                if (obj.currentStyle) {
+                    return obj.currentStyle[attr];
+                }
+                else {
+                    return document.defaultView.getComputedStyle(obj, null)[attr];
+                }
+            },
+            _getMargin(margin) {
+                let arr = margin.replace(/px/g, '').split(' ');
+                if (arr.length === 2) {
+                    arr.push(arr[0]);
+                    arr.push(arr[1]);
+                }
+                for (let i = 0; i < arr.length; i++) {
+                    arr[i] = Number(arr[i]);
+                }
+                return arr;
+            },
+            _getLeftTo() {
+                if (!this.$refs.timeLineContent) return;
+                let children = this.$refs.timeLineContent.children[0];
+                let child = children.children[0];
+                let border = Number(this._getStyle(child, 'border').split('px')[0]);
+                border = border <= 0 ? 0 : border;
+                let leftTo = child.offsetWidth + border * 2 + this._getMargin(this._getStyle(child, 'margin'))[1] + this._getMargin(this._getStyle(child, 'margin'))[3];
+                return leftTo * this.option.runNumber;
+            },
+            _prevent() {
+                if (!this.$refs.timeLineContent) return;
+                let children = this.$refs.timeLineContent.children[0];
+                this.currentIndex--;
+                this.currentIndex = this.currentIndex <= 0 ? 0 : this.currentIndex;
+                children.style.left = `${-this.currentIndex * this._getLeftTo()}px`
+            },
+            _next() {
+                if (!this.$refs.timeLineContent) return;
+                let timeLineContent = this.$refs.timeLineContent;
+                let children = timeLineContent.children[0];
+                let dis = children.offsetWidth - timeLineContent.offsetWidth;
+                this.currentIndex++;
+                if (this.currentIndex * this._getLeftTo() >= dis) {
+                    this.currentIndex = dis / this._getLeftTo();
+                }
+                children.style.left = `${-this.currentIndex * this._getLeftTo()}px`
+            },
             _initTimeLine() {
                 if (!this.$refs.timeLineContent) return;
-                let content = this.$refs.timeLineContent;
-                let children = content.children;
+                let children = this.$refs.timeLineContent.children;
                 if (children.length === 1) {
                     let child = children[0].children;
                     if (!child) return;
                     this._setContentStyle(child);
-                } else if (children.length > 1) {
-                    this._setContentStyle(children);
                 }
             },
             _setContentStyle(domArr) {
@@ -50,36 +99,60 @@
                 let timeLineLeft = this.$refs.timeLineLeft;
                 let timeLineRight = this.$refs.timeLineRight;
                 let timeLineWrapper = this.$refs.timeLineWrapper;
-                domArr[0].parentNode.style.overflow = 'hidden';
-                domArr[0].parentNode.style.position = 'relative';
                 let timeLineContentWidth = timeLineWrapper.offsetWidth - timeLineLeft.offsetWidth - timeLineRight.offsetWidth;
+                if(!timeLineContent.children[0].children[0]) return;
+                timeLineContent.style.height = `${timeLineContent.children[0].children[0].offsetHeight}px`;
+                domArr[0].parentNode.style.overflow = 'hidden';
+                domArr[0].parentNode.style.position = 'absolute';
+                domArr[0].parentNode.style.left = '0px';
+                domArr[0].parentNode.style.transition = 'all 0.3s';
+                domArr[0].parentNode.style.webkitTransition = 'all 0.3s';
                 for (let i = 0; i < domArr.length; i++) {
-                    let childrenWidth = domArr[i].offsetWidth + domArr[i].style.borderLeftWidth + domArr[i].style.borderRightWidth;
-                    domArr[i].style.position = 'absolute';
-                    domArr[i].style.left = `${(timeLineContentWidth - this.option.showNumber * childrenWidth) / (this.option.showNumber + 1)}px`;
-                    width += childrenWidth;
+                    let border = Number(this._getStyle(domArr[i], 'border').split('px')[0]);
+                    border = border <= 0 ? 0 : border;
+                    let childrenWidth = domArr[i].offsetWidth + border * 2 + this._getMargin(this._getStyle(domArr[i], 'margin'))[1] + this._getMargin(this._getStyle(domArr[i], 'margin'))[3];
+                    domArr[i].style.float = 'left';
+                    width += Number(childrenWidth);
                 }
-                timeLineContent.style.width = `${timeLineContentWidth}px`;
-                domArr[0].parentNode.style.width = `${width}px`;
+                setTimeout(() => {
+                    timeLineContent.style.width = `${timeLineContentWidth}px`;
+                    domArr[0].parentNode.style.width = `${width}px`;
+                })
             }
         },
         mounted() {
             this.$nextTick(() => {
                 this._initTimeLine();
+            });
+            window.addEventListener('resize', () => {
+                this._initTimeLine();
             })
+        },
+        watch: {
+            data() {
+                setTimeout(() => {
+                    this._initTimeLine();
+                })
+            }
         }
     }
 </script>
 
 <style scoped lang="less">
     .time_line {
+        width: 100%;
+        height: 100%;
+        position: relative;
+        overflow: hidden;
         .left, .content, .right {
             float: left;
+            user-select: none;
         }
         .left, .right {
             cursor: pointer;
         }
         .content {
+            position: relative;
             overflow: hidden;
         }
     }
--- src/components/timeLine/timeLine.vue
+++ src/components/timeLine/timeLine.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="timeLine">
-        <fx-time-line>
-            <div slot="left"><</div>
+        <fx-time-line :data="timeLineData" style="width: 758px;height: 30px;">
+            <div class="left" slot="left"><</div>
             <ul class="list" slot="content">
                 <li v-for="item in timeLineData">{{item.title}}</li>
             </ul>
-            <div slot="right">></div>
+            <div class="right" slot="right">></div>
         </fx-time-line>
     </div>
 </template>
@@ -15,7 +15,13 @@
         name: 'timeLine',
         data() {
             return {
-                timeLineData: [
+                timeLineData: [],
+                option: {}
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.timeLineData = [
                     {title: 2010},
                     {title: 2011},
                     {title: 2012},
@@ -26,23 +32,28 @@
                     {title: 2017},
                     {title: 2018},
                     {title: 2019},
-                ],
-                option: {
-                    width: 400,
-                    height: 30
-                }
-            }
+                ];
+            }, 1000)
         }
     }
 </script>
 
 <style scoped lang="less">
+    .left, .right {
+        width: 50px;
+        height: 30px;
+        line-height: 30px;
+        text-align: center;
+    }
+
     .list {
         > li {
             padding: 0 30px;
             border: 1px solid #ccc;
             height: 30px;
             line-height: 30px;
+            box-sizing: border-box;
+            margin: 0 10px;
         }
     }
 </style>
